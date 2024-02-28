c5c12aae490bf8802c368eea49a335dabe8c6d02
purplelemons-dev <ironbreaker2005@gmail.com>
2024-02-27 23:19:58 -0600

    works now with stream

--- src/bot.py
+++ src/bot.py
@@ -1,21 +1,40 @@
-
-from env import API_KEY, GUILD_ID
+from env import API_KEY, GUILD_ID, OPENAI_API_KEY, OPENAI_ORG_ID
 import discord
-from discord.ext import commands
+from openai import OpenAI
+
+openai = OpenAI(api_key=OPENAI_API_KEY, organization=OPENAI_ORG_ID)
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="/",intents=intents)
-guild = bot.get_guild(GUILD_ID)
+client = discord.Client(intents=intents)
+guild = client.get_guild(GUILD_ID)
+tree = discord.app_commands.CommandTree(client)
 
 
-@bot.tree.command(
-    name="test",
-    guild=guild,
-)
-async def test(ctx: commands.Context):
-    await ctx.send("Test")
+@tree.command(name="chat", description="Chat with GPT4!", guild=guild)
+async def chat(interaction: discord.Interaction, message: str):
+    await interaction.response.defer(ephemeral=True, thinking=True)
+    response = openai.chat.completions.create(
+        model="gpt-4-turbo-preview",
+        messages=[{"role": "user", "content": message}],
+        stream=True,
+    )
+    content = ""
+    for chunk in response:
+        chunk_content = chunk.choices[0].delta.content
+        if chunk_content:
+            content += chunk_content
+            await interaction.edit_original_response(content=content)
+        elif not chunk.choices[0].finish_reason:
+            continue
+        else:
+            break
 
 
-bot.run(API_KEY)
+@client.event
+async def on_ready():
+    await tree.sync(guild=guild)
+
+
+client.run(API_KEY)
